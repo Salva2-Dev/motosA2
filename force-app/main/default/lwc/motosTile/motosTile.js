f77cf733023ac84076be03edcf4f6ef046b7ac7c
Salva Belenguer <salva_belenguer@hotmail.es>
2023-10-19 19:04:52 +0200

    introLWC

---
+++ force-app/main/default/lwc/motosTile/motosTile.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class MotosTile extends LightningElement {
+    moto = {
+    "Imagen__c": "https://www.motofichas.com/images/phocagallery/Kawasaki/ninja-400-2023/06-kawasaki-ninja-400-2023-estudio-negro.jpg",
+    "Marca__c": "KAWASAKI",
+    "Name": "NINJA 400",
+    "Precio__c": 6999 + " €"};
+}
