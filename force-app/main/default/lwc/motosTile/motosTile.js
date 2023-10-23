cb04b1acdf22c2097f067ae966562b29830ce52a
Salva Belenguer <salva_belenguer@hotmail.es>
2023-10-23 13:03:15 +0200

    new 23-10

--- force-app/main/default/lwc/motosTile/motosTile.js
+++ force-app/main/default/lwc/motosTile/motosTile.js
@@ -1,6 +1,31 @@
 import { LightningElement, api, } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class MotosTile extends LightningElement {
+
+export default class MotosTile extends NavigationMixin(LightningElement) {
    @api moto;
 
-}
+   get Europea(){
+      return  this.moto.Procedencia__c.includes("Europea");
+   }
+
+   get Japonesa(){
+      return  this.moto.Procedencia__c.includes("Japonesa");
+   }
+
+   navigateToRecordViewPage() {
+      // View a custom object record.
+      this[NavigationMixin.Navigate]({
+        type: "standard__recordPage",
+        attributes: {
+          recordId: this.moto.Id,
+          objectApiName: "namespace__ObjectName", // objectApiName is optional
+          actionName: "view",
+        },
+      });
+    }
+
+   }
+
+
+//moto.Procedencia__c   Europea Japonesa China
